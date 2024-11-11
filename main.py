cb2c1c2070f1400aa2c5c448a5fa154030190baf
gregorygo12 <gregorygocc@gmail.com>
2024-11-11 21:44:55 +0100

    fix: getting config args

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import hydra
-from omegaconf import DictConfig, OmegaConf
+from omegaconf import DictConfig
 
 
 @hydra.main(version_base=None, config_path="configs", config_name="base")
 def main(cfg: DictConfig):
-    print(OmegaConf.to_yaml(cfg))
+
+    general = cfg.get("general")
+    for key, val in general.items():
+        print(f"Key: {key}, Val: {val}, Type: {type(val)}")
 
 
 if __name__ == "__main__":
